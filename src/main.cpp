9237bfeb7098b265b2c17ff38b9ed95f9fd9b127
Robert Jureczko <robert.jureczko@proton.me>
2023-08-26 16:30:13 +0200

    feat(garage-controller): corrected JSON outputs in endpoints

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <Arduino.h>
 #include <IPAddress.h>
 
 //network constants
-const char *ssid = ".....";
-const char *password = ".....";
-const char *remoteIp = ".....";
+const char *ssid = "*****";
+const char *password = "*****";
+const char *remoteIp = "*****";
 
 // thermistor constants
 const double VCC = 3.3;             // NodeMCU on board 3.3v vcc
-const double R2 = 2080;            // 10k ohm series resistor
+const double R2 = 2005;            // 2k ohm series resistor
 const double adc_resolution = 1023; // 10-bit adc
 const float A = 1.009249522e-03, B = 2.378405444e-04, C = 2.019202697e-07;
+
+enum Status {
+    On, Off
+};
 
 
 ESP8266WebServer server(80);
 
-const int led = 13;
-bool heating = false;
+const short HeaterPin = 0;
+const short led = 13;
+Status heatingStatus = Off;
+
+double measureTemperature();
 
 void handleRoot() {
     digitalWrite(led, 1);
@@ -29,24 +35,33 @@ void handleRoot() {
 }
 
 void Heater() {
-    if(String(server.client().remoteIP().toString().c_str()) != remoteIp){
+    if (String(server.client().remoteIP().toString().c_str()) != remoteIp) {
         digitalWrite(led, 1);
         server.send(401, "text/plain", "UnAuthorized");
         digitalWrite(led, 0);
-    } else if (server.method() != HTTP_PUT) {
+    } else if (server.method() != HTTP_PATCH) {
         digitalWrite(led, 1);
         server.send(405, "text/plain", "Method Not Allowed");
         digitalWrite(led, 0);
     } else {
         digitalWrite(led, 1);
-        server.send(200, "text/plain", "Heater: " + server.arg(0));
-
-        digitalWrite(led, 0);
+        server.send(204);
+        if (server.arg(0) == "On") {
+            heatingStatus = On;
+            digitalWrite(HeaterPin, 1);
+        } else if (server.arg(0) == "Off") {
+            heatingStatus = Off;
+            digitalWrite(HeaterPin, 0);
+        } else {
+            digitalWrite(led, 1);
+            server.send(404, "text/plain", "Wrong parameters");
+            digitalWrite(led, 0);
+        }
     }
 }
 
 void HeaterStatus() {
-    if(String(server.client().remoteIP().toString().c_str()) != remoteIp){
+    if (String(server.client().remoteIP().toString().c_str()) != remoteIp) {
         digitalWrite(led, 1);
         server.send(401, "text/plain", "UnAuthorized");
         digitalWrite(led, 0);
@@ -56,13 +71,16 @@ void HeaterStatus() {
         digitalWrite(led, 0);
     } else {
         digitalWrite(led, 1);
-        server.send(200, "application/json", "Temperature: " + String(heating));
+        String heatStatusText = "";
+        if (heatingStatus == On) heatStatusText = "On";
+        if (heatingStatus == Off) heatStatusText = "Off";
+        server.send(200, "application/json", R"({"Heating": ")" + heatStatusText + "\"" + "}");
         digitalWrite(led, 0);
     }
 }
 
 void Temperature() {
-    if(String(server.client().remoteIP().toString().c_str()) != remoteIp){
+    if (String(server.client().remoteIP().toString().c_str()) != remoteIp) {
         digitalWrite(led, 1);
         server.send(401, "text/plain", "UnAuthorized");
         digitalWrite(led, 0);
@@ -72,21 +90,26 @@ void Temperature() {
         digitalWrite(led, 0);
     } else {
         digitalWrite(led, 1);
-        double Vout, Rth, temperature, adc_value;
-        adc_value = analogRead(A0);
-        Vout = (adc_value * VCC) / adc_resolution;
-        Rth = (VCC * R2 / Vout) - R2;
-
-        /*  Steinhart-Hart Thermistor Equation:
-         *  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)
-        *  where A = 0.001129148, B = 0.000234125 and C = 8.76741*10^-8  */
-        temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)), 3))));   // Temperature in kelvin
-
-        temperature = temperature - 273.15;  // Temperature in degree celsius
-        temperature = (temperature - 32) * 0.5556;
-        server.send(200, "application/json", "Temperature: " + String(temperature));
+        double temperature = measureTemperature();
+        server.send(200, "application/json", "{\"Temperature\": " + String(temperature) + "}");
         digitalWrite(led, 0);
     }
+}
+
+double measureTemperature() {
+    double VOut, Rth, temperature, adc_value;
+    adc_value = analogRead(A0);
+    VOut = (adc_value * VCC) / adc_resolution;
+    Rth = (VCC * R2 / VOut) - R2;
+
+    /*  Steinhart-Hart Thermistor Equation:
+     *  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)
+    *  where A = 0.001129148, B = 0.000234125 and C = 8.76741*10^-8  */
+    temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)), 3))));   // Temperature in kelvin
+
+    temperature = temperature - 273.15;  // Temperature in degree celsius
+    temperature = (temperature - 32) * 0.5556;
+    return temperature;
 }
 
 void handleNotFound() {
